67cc146cc001a0638738be4b98654ba4b15a55ba
Karmanongithub <92850381+Karmanongithub@users.noreply.github.com>
2021-10-21 12:10:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(400,400);
+  var sqaure= createSprite(200,200,25,25)
+  sqaure.shapeColor= "black"
+}
+
+function draw() 
+{
+  background("lightblue");
+
+
+  drawSprites()
+}
+
+
+
+
